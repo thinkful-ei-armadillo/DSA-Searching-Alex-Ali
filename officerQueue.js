56c07d80d48321b2226319e9434f373743020f3d
AReach <DerAReich@gmail.com>
2019-04-10 13:37:24 -0500

    added breadth search

--- officerQueue.js
+++ officerQueue.js
@@ -1,4 +1,4 @@
-const BST = require("./bstSearch");
+const { BinarySearchTree } = require("./bstSearch");
 
 class _Node {
   constructor(data, next) {
@@ -32,9 +32,35 @@ class Queue {
   }
 }
 
-function commanderQueue(queue) {}
+function commanderQueue(bst, result = []) {
+  const queue = new Queue();
+  queue.enqueue(bst);
+
+  while (queue.first !== null) {
+    const node = queue.dequeue();
+    result.push(node.value);
+
+    if (node.left) {
+      queue.enqueue(node.left);
+    }
+    if (node.right) {
+      queue.enqueue(node.right);
+    }
+  }
+  result.forEach(p => console.log(p));
+}
 
 function main() {
-  let Enterprise = new BST();
-  Enterprise.insert("");
+  let Enterprise = new BinarySearchTree();
+  Enterprise.insert(5, 'Captain Picard');
+  Enterprise.insert(3, 'Commander Riker');
+  Enterprise.insert(2, 'Lt. Cmdr. Worf');
+  Enterprise.insert(4, 'Lt. Cmdr. LaForge');
+  Enterprise.insert(1, 'Lieutenant security-officer');
+  Enterprise.insert(6, 'Commander Data');
+  Enterprise.insert(8, 'Lt. Cmdr. Crusher');
+  Enterprise.insert(7, 'Lieutenant Selar');
+  commanderQueue(Enterprise);
 }
+
+main();
